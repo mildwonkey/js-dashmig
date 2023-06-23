57af81e391e7f9e946ac714972f97341829439d3
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2023-06-23 14:48:11 -0400

    it sort of does something

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/mildwonkey/js-dashmig
 
 go 1.20
 
-require github.com/spf13/cobra v1.7.0
+require (
+	github.com/bitly/go-simplejson v0.5.1
+	github.com/spf13/cobra v1.7.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
--- internal/legacydash/read.go
+++ internal/legacydash/read.go
@@ -1,7 +1,9 @@
 package legacydash
 
 import (
-	"encoding/json"
+	"fmt"
+
+	sjson "github.com/bitly/go-simplejson"
 
 	"github.com/mildwonkey/js-dashmig/internal/kinds/dashboard"
 )
@@ -9,11 +11,67 @@ import (
 func ReadDashv38(src []byte) (*dashboard.DashboardJson, error) {
 	// since this is a hack, we'll start by pretending it's fine already
 	// it's not, of course, but we'll get there
-	dash := &dashboard.DashboardJson{}
-	err := json.Unmarshal(src, dash)
+	orig, err := sjson.NewJson(src)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error from simplejson: %s", err)
 	}
 
+	// It's the real exciting work from here on out:
+	// slowly and painfully converting the old dashboard to the new format
+	// you're jealous, I know
+	dash := new(dashboard.DashboardJson)
+	var queryjson []dashboard.AnnotationQueryJson
+	annos := orig.Get("annotations").Get("list")
+
+	if annos != nil {
+		annosList, err := annos.Array()
+		if err != nil {
+			return nil, fmt.Errorf("error getting annotations list: %s", err)
+		}
+
+		queryjson = make([]dashboard.AnnotationQueryJson, len(annosList))
+
+		for i := range annosList {
+			a := annos.GetIndex(i)
+			target := a.Get("target")
+
+			// pls don't use "Musts" in prod
+			queryjson[i].Enable = a.Get("enable").MustBool()
+			queryjson[i].Name = a.Get("name").MustString()
+			typeStr := a.Get("type").MustString()
+			queryjson[i].Type = &typeStr
+			queryjson[i].IconColor = a.Get("iconColor").MustString()
+
+			ret := dashboard.AnnotationTargetJson{}
+
+			// the cue file indicates that this isn't actually required so please don't nitpick :D
+			limit, err := target.Get("target").Get("limit").Int()
+			if err != nil {
+				limit = 1
+			} else {
+				ret.Limit = limit
+			}
+			matchany, err := target.Get("target").Get("matchAny").Bool()
+			if err != nil {
+				matchany = false
+			} else {
+				ret.MatchAny = matchany
+			}
+			queryjson[i].Target = &ret
+		}
+
+		// etc, etc, etc
+	}
+	dash.Annotations = new(dashboard.AnnotationContainerJson)
+	dash.Annotations.List = queryjson
+
+	if id, err := orig.Get("id").Int(); err != nil {
+		dash.Id = &id
+	}
+	if uid, err := orig.Get("uid").String(); err != nil {
+		dash.Uid = &uid
+	}
+
+	// etc, etc, etc
 	return dash, nil
 }
